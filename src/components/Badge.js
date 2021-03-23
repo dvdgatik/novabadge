8598b527b94cc83eb79b6ec48610b0f9e040b446
David Gatica <davgatik@gmail.com>
2021-03-23 13:30:56 -0600

    Primer componente Badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,36 +1,29 @@
-import React from 'react';
+import React from 'react'; // Para usar JSX
+import confLogo from '../images/badge-header.svg'
 
-import './styles/Badge.css';
-import confLogo from '../images/badge-header.svg';
+//Los componentes son clases (o funciones)
 
 class Badge extends React.Component {
-  render() {
-    return (
-      <div className="Badge">
-        <div className="Badge__header">
-          <img src={confLogo} alt="Logo de la conferencia" />
-        </div>
-
-        <div className="Badge__section-name">
-          <img
-            className="Badge__avatar"
-            src={this.props.avatarUrl}
-            alt="Avatar"
-          />
-          <h1>
-            {this.props.firstName} <br /> {this.props.lastName}
-          </h1>
-        </div>
-
-        <div className="Badge__section-info">
-          <h3>{this.props.jobTitle}</h3>
-          <div>@{this.props.twitter}</div>
-        </div>
-
-        <div className="Badge__footer">#platziconf</div>
-      </div>
-    );
-  }
+   render() {
+       return(
+           <div>
+             <img src={confLogo} alt='Logo de la conferencia'/>
+             <div>
+               <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
+               <h1>
+                 David <br/> Gatica
+               </h1>
+             </div>
+             <div>
+               <p>Frontend Engineer</p>
+               <p>@dvdgatik</p>
+             </div>
+             <div>
+               #novaconf
+             </div>
+           </div>
+         )
+   }
 }
 
 export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,9 @@
-// // Crear un elemento
-// const element = document.createElement('h1');
-
-// // Insertar texto
-// element.innerText = 'Hello, Platzi Badges'
-
-// // Donde queremos colocarlo?
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
- 
-
-
-import React from 'react'; // Nos permite usar JSX y usar REACT
+import React from 'react';
 import ReactDOM from 'react-dom';
+import Badge from './components/Badge'
 
-//const jsx = <h1>Hello, Platzi Badges from React </h1>
-
-//const element = React.createElement('a', {href: 'https://www.platzi.com'}, 'Ir a platzi');
-//Create element tiene tres parámetros
-//el elemento
-//sus atributos
-//el children (parecido al innerText o innerHTML)
-
-//const name = 'David'
-//const sum = () => 3+3
-//const element = React.createElement('h1', {}, `Hola soy ${name}`);
-
-//const jsx = <h1> hola soy, {undefined} </h1>
-
-const jsx = 
-<div>
-	<h1>Hola, soy David</h1>
-	<p>Soy un ingeniero frontend.</p>
-</div>
-
-const element = React.createElement(
-	'div', 
-	{},
-	React.createElement('h1', {}, 'Hola soy Daniel'),
-	React.createElement('p', {}, 'Soy un Ingeniero frontend')
-)
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__)
 
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge/>, container);
