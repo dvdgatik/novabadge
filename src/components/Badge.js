271f604729437648ae0cb4d71e4b6292303da45f
David Gatica <davgatik@gmail.com>
2021-03-26 00:20:33 -0600

    Uso de Props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,24 +1,28 @@
 import React from 'react'; // Para usar JSX
-import confLogo from '../images/badge-header.svg'
+import confLogo from '../images/badge-header.svg';
+import './styles/Badge.css';
 
 //Los componentes son clases (o funciones)
 
 class Badge extends React.Component {
    render() {
+
        return(
-           <div>
-             <img src={confLogo} alt='Logo de la conferencia'/>
-             <div>
-               <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
+           <div className='Badge'>
+             <div className='Badge__header'>
+               <img src={confLogo} alt='Logo de la conferencia'/>
+             </div>             
+             <div className='Badge__section-name'>
+               <img className='Badge__avatar' src={this.props.avatar_url} alt="Avatar"/>
                <h1>
-                 David <br/> Gatica
+                 {this.props.firstName} <br/> {this.props.lastName}
                </h1>
              </div>
-             <div>
-               <p>Frontend Engineer</p>
-               <p>@dvdgatik</p>
+             <div className='Badge__section-info'>
+               <h3>{this.props.jobTitle}</h3>
+               <div>@{this.props.twitter}</div>
              </div>
-             <div>
+             <div className='Badge__footer'>
                #novaconf
              </div>
            </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Badge from './components/Badge'
+import 'bootstrap/dist/css/bootstrap.css'; // archivo de node_modules
+import './global.css';
+import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__)
 
-ReactDOM.render(<Badge/>, container);
+ReactDOM.render(<Badge 
+	firstName='Lilly' 
+	lastName='Kaufan' 
+	jobTitle='Front End' 
+	twitter='dvdgatik'
+	avatar_url='https://www.gravatar.com/avatar?d=identicon'
+
+	/>, container);
