d885ac54a05ead88258eb5907bd9ea5d91e2c2ca
David Gatica <davgatik@gmail.com>
2021-03-28 23:18:47 -0600

    Lista de componentes

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,46 +1,32 @@
 import React from 'react';
 
-import './styles/BadgesList.css';
-
-class BadgesListItem extends React.Component {
-  render() {
-    return (
-      <div className="BadgesListItem">
-        <img
-          className="BadgesListItem__avatar"
-          src={this.props.badge.avatarUrl}
-          alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
-        />
-
-        <div>
-          <strong>
-            {this.props.badge.firstName} {this.props.badge.lastName}
-          </strong>
-          <br />@{this.props.badge.twitter}
-          <br />
-          {this.props.badge.jobTitle}
-        </div>
-      </div>
-    );
-  }
-}
 
 class BadgesList extends React.Component {
+
   render() {
+
     return (
-      <div className="BadgesList">
         <ul className="list-unstyled">
-          {this.props.badges.map(badge => {
-            return (
-              <li key={badge.id}>
-                <BadgesListItem badge={badge} />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
+         {/* Por cada uno de los elementos que tenga en data convertirlos de un objeto a elementos
+         con el metodo map
+         map recibe una funcion como argumento y tiene que regresar otro valor
+
+         this.state.data.map or this.props.badges.data.map
+          */}
+         {this.props.badges.map((badge)=> {
+           return(
+             <li key={badge.id}>
+               {badge.firstName} {badge.lastName}
+             </li>
+             )
+
+         })}
+               
+         </ul>
+      );
   }
+
+
 }
 
 export default BadgesList;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import 'bootstrap/dist/css/bootstrap.css'; // archivo de node_modules
 import './global.css';
 import Badge from './components/Badge';
 import BadgeNew from './pages/BadgeNew';
+import Badges from './pages/Badges';
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__)
 
-ReactDOM.render(<BadgeNew/>, container);
+ReactDOM.render(<Badges/>, container);
